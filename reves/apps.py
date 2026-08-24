b499650905aa2c77a35a2ef2bbf3abf7f0b1f13a
narkakaba-lgtm <narkakaba@gmail.com>
2026-08-24 14:47:18 +0200

    Update apps.py

--- reves/apps.py
+++ reves/apps.py
@@ -3,3 +3,6 @@ from django.apps import AppConfig
 class RevesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'reves'
+    
+    # Adiciona um nome amigável e profissional para o painel administrativo do Django
+    verbose_name = 'Gestion des Rêves 🌙'
